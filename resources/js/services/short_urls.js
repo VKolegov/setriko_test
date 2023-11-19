4ac52bf803ffde8f61f3b4f43397dd17900b8b62
Vladislav Kolegov <dev@vkolegov.ru>
2023-11-19 21:33:00 +0700

    [JS] services/short_urls: typehinting

--- resources/js/services/short_urls.js
+++ resources/js/services/short_urls.js
@@ -1,3 +1,9 @@
 import API from './API.js';
 
-export default new API('/api/short_urls');
+/**
+ *
+ * @type {API<ShortURL>}
+ */
+const shortUrlsAPI = new API('/api/short_urls');
+
+export default shortUrlsAPI;

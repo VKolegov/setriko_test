00e0e8cc776b4ecaf7754d177236602cba5eb21d
Vladislav Kolegov <dev@vkolegov.ru>
2023-11-19 21:37:50 +0700

    [Resources] ShortURLResource

---
+++ app/Http/Resources/ShortURLResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\ShortURL;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShortURLResource extends JsonResource
+{
+    /**
+     * @var ShortURL
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'              => $this->resource->id,
+            'slug'            => $this->resource->slug,
+            'destination_url' => $this->resource->destination_url,
+            'hits'            => $this->resource->hits,
+            'name'            => $this->resource->name,
+            'created_at'      => $this->resource->created_at?->toIso8601String(),
+        ];
+    }
+}

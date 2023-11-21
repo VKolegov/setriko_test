f24eb01d1f7fd33d9a9d5f3edb03944423574041
Vladislav Kolegov <dev@vkolegov.ru>
2023-11-21 16:14:40 +0700

    vite.config.js: VITE_HMR_HOST

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,44 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 
-export default defineConfig({
-  server: {
-    hmr: {
-      host: '192.168.0.11', // TODO: parametrize
-    },
-  },
-  alias: {
-    '@': path.resolve(__dirname, './resources/js'),
-  },
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          // The Vue plugin will re-write asset URLs, when referenced
-          // in Single File Components, to point to the Laravel web
-          // server. Setting this to `null` allows the Laravel plugin
-          // to instead re-write asset URLs to point to the Vite
-          // server instead.
-          base: null,
+export default defineConfig(({ mode }) => {
 
-          // The Vue plugin will parse absolute URLs and treat them
-          // as absolute paths to files on disk. Setting this to
-          // `false` will leave absolute URLs un-touched so they can
-          // reference assets in the public directory as expected.
-          includeAbsolute: false,
-        },
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      hmr: {
+        host: env.VITE_HMR_HOST || 'localhost',
       },
-    }),
-  ],
+    },
+    alias: {
+      '@': path.resolve(__dirname, './resources/js'),
+    },
+    plugins: [
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
+      vue({
+        template: {
+          transformAssetUrls: {
+            // The Vue plugin will re-write asset URLs, when referenced
+            // in Single File Components, to point to the Laravel web
+            // server. Setting this to `null` allows the Laravel plugin
+            // to instead re-write asset URLs to point to the Vite
+            // server instead.
+            base: null,
+
+            // The Vue plugin will parse absolute URLs and treat them
+            // as absolute paths to files on disk. Setting this to
+            // `false` will leave absolute URLs un-touched so they can
+            // reference assets in the public directory as expected.
+            includeAbsolute: false,
+          },
+        },
+      }),
+    ],
+  };
 });

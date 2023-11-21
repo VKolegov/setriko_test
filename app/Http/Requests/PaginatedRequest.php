97a243055b10c5753890de8d22120e3ffa0a17bc
Vladislav Kolegov <dev@vkolegov.ru>
2023-11-21 17:15:43 +0700

    code reformat

--- app/Http/Requests/PaginatedRequest.php
+++ app/Http/Requests/PaginatedRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class PaginatedRequest extends FormRequest
@@ -17,12 +18,12 @@ class PaginatedRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'page' => ['int', 'min:1'],
+            'page'    => ['int', 'min:1'],
             'perPage' => ['int', 'min:5'],
         ];
     }
--- resources/js/pages/utils.js
+++ resources/js/pages/utils.js
@@ -4,18 +4,18 @@
  * @returns {Promise<void>}
  * @credits https://stackoverflow.com/a/65996386
  */
-export async function copyToClipboard(textToCopy) {
+export async function copyToClipboard (textToCopy) {
   // Navigator clipboard api needs a secure context (https)
   if (navigator.clipboard && window.isSecureContext) {
     await navigator.clipboard.writeText(textToCopy);
   } else {
     // Use the 'out of viewport hidden text area' trick
-    const textArea = document.createElement("textarea");
+    const textArea = document.createElement('textarea');
     textArea.value = textToCopy;
 
     // Move textarea out of the viewport so it's not visible
-    textArea.style.position = "absolute";
-    textArea.style.left = "-999999px";
+    textArea.style.position = 'absolute';
+    textArea.style.left = '-999999px';
 
     document.body.prepend(textArea);
     textArea.select();

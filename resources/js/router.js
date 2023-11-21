18965ac4041cf36b7e1a8b2da0c6aca4a13e4314
Vladislav Kolegov <dev@vkolegov.ru>
2023-11-21 15:29:31 +0700

    [Vue] router: page titles

--- resources/js/router.js
+++ resources/js/router.js
@@ -8,33 +8,53 @@ import edit from '@/pages/manage/edit.vue';
  *
  * @type {import('vue-router').RouteRecordRaw[]}
  */
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'index',
     component: index,
+    meta: {
+      title: '',
+    }
   },
   {
     path: '/manage',
     name: 'manage',
     component: list,
+    meta: {
+      title: 'Управление',
+    }
   },
   {
     path: '/manage/:id(\\d+)',
     name: 'edit',
     component: edit,
     props: true,
+    meta: {
+      title: 'Редактирование',
+    }
   },
   {
     path: '/manage/create',
     name: 'create',
     component: edit,
+    meta: {
+      title: 'Создание',
+    }
   },
 ];
 
 const router = createRouter({
   history: createWebHistory('/'),
   routes: routes,
+});
+
+let defaultTitle = document.title;
+
+router.afterEach((route) => {
+  if (route.meta.title) {
+    document.title = `${defaultTitle} | ${route.meta.title}`;
+  }
 });
 
 export default router;
